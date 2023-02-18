a6e11e3a3f54c3bc2f8e689e4cf1a8dc13b89435
ozelmel <69895481+ozelmel@users.noreply.github.com>
2023-02-18 16:42:00 +0300

    v1 added

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../pages/Login";
+import Main from "../pages/Main";
+import MovieDetail from "../pages/MovieDetail";
+import Register from "../pages/Register";
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/details/:id" element={<MovieDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
