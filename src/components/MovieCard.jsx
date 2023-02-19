00ce1c1810e7afa17b796b1ede617601e6d75bee
ozelmel <69895481+ozelmel@users.noreply.github.com>
2023-02-19 12:27:54 +0300

    MovieDetail v1

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
@@ -7,6 +8,7 @@ const defaultImage =
 
 const MovieCard = ({ poster_path, title, overview, vote_average, id }) => {
   const {currentUser} = useContext(AuthContext);
+  const navigate = useNavigate()
   const setVoteClass = (vote) => {
     if (vote > 8) {
       return "green";
@@ -17,7 +19,7 @@ const MovieCard = ({ poster_path, title, overview, vote_average, id }) => {
     }
   };
   return (
-    <div className="movie">
+    <div className="movie" onClick={() => navigate("/details/"+id)}>
       <img src={poster_path ? IMG_API + poster_path : defaultImage} alt="" />
       <div className="d-flex align-items-baseline justify-content-between p-1 text-white">
         <h5>{title}</h5>
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 const MovieDetail = () => {
-  return (
-    <div>MovieDetail</div>
-  )
-}
+  const [movieDetails, setMovieDetails] = useState({})
+  const { id } = useParams();
+  // const {title, poster_path, overview, vote_average, release_date, vote_count} = movieDetails;
+  const API_KEY = process.env.REACT_APP_TMDB_KEY;
+  const movieDetailBaseUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
+  const baseImageUrl = "https://image.tmdb.org/t/p/w1280";
+  const defaultImage =
+    "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
 
-export default MovieDetail
+  useEffect(() => {
+    axios
+      .get(movieDetailBaseUrl)
+      .then((res) => setMovieDetails(res.data))
+      .catch((err) => console.log(err));
+  }, [movieDetailBaseUrl]);
+
+  return (
+     <div className="container py-5">
+      <h1 className="text-center">{movieDetails?.title}</h1>
+      <div className="card mb-3">
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img
+              src={
+                movieDetails?.poster_path
+                  ? baseImageUrl + movieDetails?.poster_path
+                  : defaultImage
+              }
+              className="img-fluid rounded-start"
+              alt="..."
+            />
+          </div>
+          <div className="col-md-8 d-flex flex-column ">
+            <div className="card-body">
+              <h5 className="card-title">Overview</h5>
+              <p className="card-text">{movieDetails?.overview}</p>
+            </div>
+            <ul className="list-group ">
+              <li className="list-group-item">
+                {"Release Date : " + movieDetails?.release_date}
+              </li>
+              <li className="list-group-item">
+                {"Rate : " + movieDetails?.vote_average}
+              </li>
+              <li className="list-group-item">
+                {"Total Vote : " + movieDetails?.vote_count}
+              </li>
+              <li className="list-group-item">
+                <Link to={-1} className="card-link">
+                  Go Back
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );;
+};
+
+export default MovieDetail;
