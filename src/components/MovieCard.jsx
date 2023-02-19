cc7ba7494cce39b485aab857a663ac20dceb6399
ozelmel <69895481+ozelmel@users.noreply.github.com>
2023-02-19 11:31:44 +0300

    moviecard added

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
-const MovieCard = () => {
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const defaultImage =
+  "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
+
+const MovieCard = ({ poster_path, title, overview, vote_average, id }) => {
+  const {currentUser} = useContext(AuthContext);
+  const setVoteClass = (vote) => {
+    if (vote > 8) {
+      return "green";
+    } else if (vote >= 6) {
+      return "orange";
+    } else {
+      return "red";
+    }
+  };
   return (
-    <div>MovieCard</div>
-  )
-}
+    <div className="movie">
+      <img src={poster_path ? IMG_API + poster_path : defaultImage} alt="" />
+      <div className="d-flex align-items-baseline justify-content-between p-1 text-white">
+        <h5>{title}</h5>
+        {currentUser && (<span className={`tag ${setVoteClass(vote_average)}`}>
+          {vote_average}
+        </span>)}
+      </div>
+      <div className="movie-over">
+        <h2>Overview</h2>
+        <p>{overview}</p>
+      </div>
+    </div>
+  );
+};
 
-export default MovieCard
+export default MovieCard;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
+
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+// const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
-}
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
 
-export default Main
+  const getMovies = (API) => {
+    setLoading(true);
+    axios
+      .get(API)
+      .then((res) => setMovies(res.data.results))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  };
+  return (
+    <div className="d-flex justify-content-center flex-wrap">
+      {loading ? (
+        <div className="spinner-border text-primary m-5" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+      ) : (
+          movies?.map((movie) => <MovieCard key={movie.id} {...movie} />)
+      )}
+    </div>
+  );
+};
+
+export default Main;
