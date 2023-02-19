b1262aabace2dcc66884000c570a1c66243f202c
ozelmel <69895481+ozelmel@users.noreply.github.com>
2023-02-19 11:56:43 +0300

    search v2

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
+import { AuthContext } from "../context/AuthContext";
 
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
@@ -10,6 +11,7 @@ const Main = () => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
+  const { currentUser } = useContext(AuthContext);
 
   useEffect(() => {
     getMovies(FEATURED_API);
@@ -24,11 +26,15 @@ const Main = () => {
       .finally(() => setLoading(false));
   };
   const handleSubmit = (e) => {
-    e.preventDefault()
-    getMovies(SEARCH_API+searchTerm)
-
-
-  }
+    e.preventDefault();
+    if (searchTerm && currentUser) {
+      getMovies(SEARCH_API + searchTerm);
+    } else if (!currentUser) {
+      alert("Please login to search a movie...");
+    } else {
+      alert("Please enter a text...");
+    }
+  };
   return (
     <>
       <form className="search" onSubmit={handleSubmit}>
